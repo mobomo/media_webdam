20a3f18eb9619ab0bef8e20e22894a6fc451f2b6
Cameron Eagans <me@cweagans.net>
2017-06-27 16:29:29 -0600

    Add a placeholder test

---
+++ src/Tests/PlaceholderTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\media_webdam\Tests;
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Placeholder test. Delete when there are real tests.
+ *
+ * @group media_webdam
+ */
+class PlaceholderTest extends WebTestBase {
+
+  public static $modules = [
+    'media_webdam',
+  ];
+
+  /**
+   * A test that always returns true.
+   */
+  public function testPlaceholder() {
+    $this->assertTrue(TRUE);
+  }
+
+}
