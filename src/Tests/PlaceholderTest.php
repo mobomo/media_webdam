389c9fcd5384215999b176a9be55850d6dddc894
Cameron Eagans <me@cweagans.net>
2017-06-27 17:59:18 -0600

    Maybe Travis will be happier with a Kernel test?

--- src/Tests/PlaceholderTest.php
+++ src/Tests/PlaceholderTest.php
@@ -2,18 +2,14 @@
 
 namespace Drupal\media_webdam\Tests;
 
-use Drupal\simpletest\WebTestBase;
+use Drupal\simpletest\KernelTestBase;
 
 /**
  * Placeholder test. Delete when there are real tests.
  *
  * @group media_webdam
  */
-class PlaceholderTest extends WebTestBase {
-
-  public static $modules = [
-    'media_webdam',
-  ];
+class PlaceholderTest extends KernelTestBase {
 
   /**
    * A test that always returns true.
