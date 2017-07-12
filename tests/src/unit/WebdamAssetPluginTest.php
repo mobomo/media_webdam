63374aeacebec42b159a6241a605d303784ff8ac
Cameron Eagans <me@cweagans.net>
2017-07-11 20:43:22 -0600

    Test getField()

--- tests/src/unit/WebdamAssetPluginTest.php
+++ tests/src/unit/WebdamAssetPluginTest.php
@@ -5,6 +5,7 @@ namespace Drupal\Tests\media_webdam\unit;
 use Drupal\Core\Config\Config;
 use Drupal\Core\Entity\EntityFieldManager;
 use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\media_entity\Entity\Media;
 use Drupal\media_webdam\Plugin\MediaEntity\Type\WebdamAsset;
 use Drupal\Tests\UnitTestCase;
 
@@ -29,6 +30,24 @@ class WebdamAssetPluginTest extends UnitTestCase {
     );
 
     $this->assertArrayHasKey('type', $plugin->providedFields());
+  }
+
+  /**
+   * Tests the getField method.
+   */
+  public function testGetField() {
+    $plugin = new WebdamAsset(
+      array(),
+      'test_plugin',
+      array(),
+      $this->getMock(EntityTypeManager::class),
+      $this->getMock(EntityFieldManager::class),
+      $this->getMock(Config::class)
+    );
+
+    $this->assertEquals('image', $plugin->getField(
+      $this->getMock(Media::class), 'type')
+    );
   }
 
 }
