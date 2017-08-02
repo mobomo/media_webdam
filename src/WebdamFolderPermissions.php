b84236f48114e2f8d27c65a74cfe6555cf270408
Brian Wagner <briwagner@outlook.com>
2017-08-02 13:52:46 -0500

    move folder action to separate function

--- src/WebdamFolderPermissions.php
+++ src/WebdamFolderPermissions.php
@@ -20,6 +20,9 @@ class WebdamFolderPermissions implements ContainerInjectionInterface {
     $this->webdam = $webdam;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
@@ -28,36 +31,47 @@ class WebdamFolderPermissions implements ContainerInjectionInterface {
   }
 
   public function getPermissions() {
-    // Get list of folders on Webdam.
-    $webdamFolders = $this->webdam->getFlattenedFolderList();
-
-    // Get local configuration for Webdam folders.
-    $allFolders = $this->config->get('folders_filter');
+    $permissions = [];
 
     // Eliminate folders NOT enabled via WebdamConfig.
-    $enabledFolders = array_filter($allFolders, function($v) { return $v != 0;});
+    $enabledFolders = $this->getEnabledFolders();
 
-    // Build permissions for each enabled folder.
-    $permissions = [];
-    foreach ($enabledFolders as $k => $v) {
-      $permissions['view ' . $k] = [
-        'title' => $this->t('<em>@name</em>: View', ['@name' => $webdamFolders[$k]]),
-        'description' => $this->t('View assets in folder @name.', ['@name' => $webdamFolders[$k]]),
-      ];
-      $permissions['create ' . $k] = [
-        'title' => $this->t('<em>@name</em>: Create', ['@name' => $webdamFolders[$k]]),
-        'description' => $this->t('Create assets in folder @name.', ['@name' => $webdamFolders[$k]]),
-      ];
-      $permissions['update ' . $k] = [
-        'title' => $this->t('<em>@name</em>: Update', ['@name' => $webdamFolders[$k]]),
-        'description' => $this->t('Update assets to folder @name.', ['@name' => $webdamFolders[$k]]),
-      ];
-      $permissions['delete ' . $k] = [
-        'title' => $this->t('<em>@name</em>: Delete', ['@name' => $webdamFolders[$k]]),
-        'description' => $this->t('Delete assets in folder @name.', ['@name' => $webdamFolders[$k]]),
-      ];
+    if (!empty($enabledFolders)) {
+      // Get list of folders on Webdam.
+      $webdamFolders = $this->webdam->getFlattenedFolderList();
+
+      // Build permissions for each enabled folder.
+      foreach ($enabledFolders as $k => $v) {
+        $permissions['view ' . $k] = [
+          'title' => $this->t('<em>@name</em>: View', ['@name' => $webdamFolders[$k]]),
+          'description' => $this->t('View assets in folder @name.', ['@name' => $webdamFolders[$k]]),
+        ];
+        $permissions['create ' . $k] = [
+          'title' => $this->t('<em>@name</em>: Create', ['@name' => $webdamFolders[$k]]),
+          'description' => $this->t('Create assets in folder @name.', ['@name' => $webdamFolders[$k]]),
+        ];
+        $permissions['update ' . $k] = [
+          'title' => $this->t('<em>@name</em>: Update', ['@name' => $webdamFolders[$k]]),
+          'description' => $this->t('Update assets to folder @name.', ['@name' => $webdamFolders[$k]]),
+        ];
+        $permissions['delete ' . $k] = [
+          'title' => $this->t('<em>@name</em>: Delete', ['@name' => $webdamFolders[$k]]),
+          'description' => $this->t('Delete assets in folder @name.', ['@name' => $webdamFolders[$k]]),
+        ];
+      }
     }
 
     return $permissions;
+  }
+
+  /**
+   * Return array of Webdam folders set to enabled in local configuration. 
+   */
+  public function getEnabledFolders() {
+    // Get local configuration for Webdam folders.
+    $allFolders = $this->config->get('folders_filter');
+    // Eliminate folders NOT enabled via WebdamConfig.
+    $enabledFolders = array_filter($allFolders, function($v) { return $v != 0;});
+    return $enabledFolders;
   }
 }
