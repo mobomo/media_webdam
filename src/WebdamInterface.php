6456f063b8835bae8edaa8335008df1bc42e748e
Cameron Eagans <me@cweagans.net>
2017-09-05 12:55:08 -0600

    Clean up the Webdam service

--- src/WebdamInterface.php
+++ src/WebdamInterface.php
@@ -19,14 +19,6 @@ namespace Drupal\media_webdam;
 interface WebdamInterface {
 
   /**
-   * Get details about the current Webdam account's subscription.
-   *
-   * @return \stdClass
-   *   Subscription details.
-   */
-  public function getSubscriptionDetails();
-
-  /**
    * Get a list of folders keyed by ID.
    *
    * @param int $folder_id
@@ -38,29 +30,15 @@ interface WebdamInterface {
   public function getFlattenedFolderList($folder_id = NULL);
 
   /**
-   * Gets a webdam folder by its ID.
+   * Passes method calls through to the webdam client object.
    *
-   * @param int $folder_id
-   *   The folder ID to recurse into. This is mostly for internal use.
+   * @param $name
+   *   The name of the method to call.
+   * @param $arguments
+   *   An array of arguments.
    *
-   * @return object
-   *   Webdam folder.
+   * @return mixed
+   *   Returns whatever the webdam client returns.
    */
-  public function getFolder($folder_id = NULL);
-
-  /**
-   * Uploads Assets to Webdam using the previously defined methods.
-   *
-   * @param string $file_uri
-   *   The file URI.
-   *  @param string $file_name
-   *   The File filename.
-   * @param int $folderID
-   *   The Webdam folder ID.
-   *
-   * @return array
-   *   Webdam response.
-   */
-  public function uploadAsset($file_uri, $file_name, $folderID = NULL);
-
+  public function __call($name, $arguments);
 }
