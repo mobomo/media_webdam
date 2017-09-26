12478c238cc3f793585a84ebd680d013c589a73c
anairamzap-mobomo <mariana@mobomo.com>
2017-09-26 14:31:02 -0300

    Webdam-73 - Reformat code following Drupal Standards. WebdamAsset class

--- src/Plugin/MediaEntity/Type/WebdamAsset.php
+++ src/Plugin/MediaEntity/Type/WebdamAsset.php
@@ -26,20 +26,29 @@ use Drupal\file\Entity\File;
 class WebdamAsset extends MediaTypeBase {
 
   /**
+   * A configured webdam API object.
+   *
    * @var \Drupal\media_webdam\Webdam $webdam
    */
   protected $webdam;
 
   /**
+   * The asset that we're going to render details for.
+   *
    * @var \cweagans\webdam\Entity\Asset
    */
   protected $asset = NULL;
 
   /**
+   * The file entity.
+   *
    * @var \Drupal\file\Entity\File
    */
   protected $file = NULL;
 
+  /**
+   * WebdamAsset constructor.
+   */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager, ConfigFactoryInterface $config_factory, WebdamInterface $webdam) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $entity_field_manager, $config_factory->get('media_entity.settings'));
     $this->webdam = $webdam;
@@ -59,7 +68,6 @@ class WebdamAsset extends MediaTypeBase {
       $container->get('media_webdam.webdam')
     );
   }
-
 
   /**
    * {@inheritdoc}
@@ -127,40 +135,54 @@ class WebdamAsset extends MediaTypeBase {
     if (is_null($assetID)) {
       return FALSE;
     }
-    // If the webdam asset has not been loaded
-    if(!$this->asset){
+    // If the webdam asset has not been loaded.
+    if (!$this->asset) {
       // Load the asset.
       $this->asset = $this->webdam->getAsset($assetID);
     }
     switch ($name) {
       case 'type_id':
         return $this->asset->type_id;
+
       case 'filename':
         return $this->asset->filename;
+
       case 'filesize':
         return $this->asset->filesize;
+
       case 'width':
         return $this->asset->width;
+
       case 'height':
         return $this->asset->height;
+
       case 'description':
         return $this->asset->description;
+
       case 'filetype':
         return $this->asset->filetype;
+
       case 'colorspace':
         return $this->asset->colorspace;
+
       case 'version':
         return $this->asset->version;
+
       case 'datecreated':
         return $this->asset->date_created_unix;
+
       case 'datemodified':
         return $this->asset->date_modified_unix;
+
       case 'datecaptured':
         return $this->asset->datecapturedUnix;
+
       case 'folderID':
         return $this->asset->folder->id;
+
       case 'file':
         return $this->file ? $this->file->id() : NULL;
+
       case 'status':
         return $this->asset->status == 'active';
     }
@@ -172,19 +194,19 @@ class WebdamAsset extends MediaTypeBase {
    * {@inheritdoc}
    */
   public function thumbnail(MediaInterface $media) {
-    //Load the bundle for this asset
+    // Load the bundle for this asset.
     $bundle = $this->entityTypeManager->getStorage('media_bundle')->load($media->bundle());
-    //Load the field definitions for this bundle
+    // Load the field definitions for this bundle.
     $field_definitions = $this->entityFieldManager->getFieldDefinitions($media->getEntityTypeId(), $media->bundle());
-    //If a source field is set for this bundle
+    // If a source field is set for this bundle.
     if (isset($this->configuration['source_field'])) {
-      //Set the name of the source field
+      // Set the name of the source field.
       $source_field = $this->configuration['source_field'];
-      //If the media entity has the source field
+      // If the media entity has the source field.
       if ($media->hasField($source_field)) {
-        //Set the property name for the source field
+        // Set the property name for the source field.
         $property_name = $media->{$source_field}->first()->mainPropertyName();
-        //Get the webdam asset ID value from the source field
+        // Get the webdam asset ID value from the source field.
         $assetID = $media->{$source_field}->{$property_name};
       }
     }
@@ -194,67 +216,70 @@ class WebdamAsset extends MediaTypeBase {
     }
     // Load the asset.
     $asset = $this->webdam->getAsset($assetID);
-    //Download the webdam asset file as a string
+    // Download the webdam asset file as a string.
     $file_contents = $this->webdam->downloadAsset($asset->id);
-    //Set the path for webdam assets.
-    //If the bundle has a field mapped for the file
-    if($file_field = $bundle->field_map['file']){
-      //Get the storage scheme for the file field
+    // Set the path for webdam assets.
+    // If the bundle has a field mapped for the file.
+    if ($file_field = $bundle->field_map['file']) {
+      // Get the storage scheme for the file field.
       $scheme = $field_definitions[$file_field]->getItemDefinition()->getSetting('uri_scheme');
-    }else{
-      //Otherwise default to public storage
+    }
+    else {
+      // Otherwise default to public storage.
       $scheme = 'public';
     }
-    //Set the path prefix for the file that is about to be downloaded from webdam and saved in to Drupal
+    // Set the path prefix for the file that is about to be downloaded from
+    // webdam and saved in to Drupal.
     $path = $scheme . '://webdam_assets/';
-    //Prepare webdam directory for writing and only proceed if successful
-    if(file_prepare_directory($path,FILE_CREATE_DIRECTORY)) {
-      //Save the file into Drupal
+    // Prepare webdam directory for writing and only proceed if successful.
+    if (file_prepare_directory($path, FILE_CREATE_DIRECTORY)) {
+      // Save the file into Drupal.
       $file = file_save_data($file_contents, $path . $asset->id . '.' . $asset->filetype, FILE_EXISTS_REPLACE);
-      //If the file was saved
+      // If the file was saved.
       if ($file instanceof FileInterface || $file instanceof File) {
         $this->file = $file;
-        //Get the mimetype of the file
+        // Get the mimetype of the file.
         $mimetype = $file->getMimeType();
-        //Split the mimetype into 2 parts (primary/secondary)
+        // Split the mimetype into 2 parts (primary/secondary)
         $mimetype = explode('/', $mimetype);
-        //If the primary mimetype is not an image
+        // If the primary mimetype is not an image.
         if ($mimetype[0] != 'image') {
-          //Try to get the icon for this type of file using both primary and secondary mimetype
+          // Try to get the filetype icon using primary and secondary mimetype.
           $thumbnail = $this->config->get('icon_base') . "/{$mimetype[0]}-{$mimetype[1]}.png";
-          //If icon is not found
+          // If icon is not found.
           if (!is_file($thumbnail)) {
-            //Try to get the icon for this type of file using only the secondary mimetype
+            // Try to get the filetype icon using only the secondary mimetype.
             $thumbnail = $this->config->get('icon_base') . "/{$mimetype[1]}.png";
-            //If icon is still not found
+            // If icon is still not found.
             if (!is_file($thumbnail)) {
-              //Use a generic document icon
+              // Use a generic document icon.
               $thumbnail = $this->config->get('icon_base') . '/document.png';
             }
           }
-        } else {
-          //load the image
+        }
+        else {
+          // Load the image.
           $image = \Drupal::service('image.factory')->get($file->getFileUri());
           /** @var \Drupal\Core\Image\Image $image */
-          //If the image is valid
-          if($image->isValid()){
-            //Load all image styles
+          // If the image is valid.
+          if ($image->isValid()) {
+            // Load all image styles.
             $styles = ImageStyle::loadMultiple();
-            //For each image style
-            foreach ($styles as $style){
+            // For each image style.
+            foreach ($styles as $style) {
               /** @var ImageStyle $style */
-              //Flush and regenerate the styled image
+              // Flush and regenerate the styled image.
               $style->flush($file->getFileUri());
             }
           }
-          //Use the URI of the image
+          // Use the URI of the image.
           $thumbnail = $file->getFileUri();
         }
-        //Return the file URI
+        // Return the file URI.
         return $thumbnail;
       }
     }
-    //If the file field is not mapped, use the default webdam icon
+    // If the file field is not mapped, use the default webdam icon.
     return drupal_get_path('module', 'media_webdam') . '/img/webdam.png';
   }
 
